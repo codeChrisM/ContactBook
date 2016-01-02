4b16339c0d73ba9e41eb648aca17add8232fb615
keyFrameM <marlowe.animation@gmail.com>
2016-01-02 09:28:21 -0600

    added the Friends Loader

--- app/src/main/java/com/codechris/friends/Friend.java
+++ app/src/main/java/com/codechris/friends/Friend.java
@@ -17,4 +17,36 @@ public class Friend {
         this.phone = phone;
         this.email = email;
     }
+
+    public int getId() {
+        return _id;
+    }
+
+    public void setId(int _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
